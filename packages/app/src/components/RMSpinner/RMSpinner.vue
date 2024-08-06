2b1321faf8754ebe092fb844f6dfc19a5204624a
takamitsu0124 <oga.nextiv@gmail.com>
2024-08-07 01:27:01 +0900

    【 app - component 】RMSpinner Fixes #17

---
+++ packages/app/src/components/RMSpinner/RMSpinner.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="_spinner_container">
+    <div class="_loading_box">
+      <span class="_loading_item" style="--index: 0"></span>
+      <span class="_loading_item" style="--index: 1"></span>
+      <span class="_loading_item" style="--index: 2"></span>
+      <span class="_loading_item" style="--index: 3"></span>
+      <span class="_loading_item" style="--index: 4"></span>
+      <span class="_loading_item" style="--index: 5"></span>
+      <span class="_loading_item" style="--index: 6"></span>
+      <span class="_loading_item" style="--index: 7"></span>
+    </div>
+  </div>
+</template>
+<style lang="sass" scoped>
+._spinner_container
+  display: grid
+  place-items: center
+  position: fixed
+  top: 0
+  left: 0
+  width: 100vw
+  height: 100vh
+  z-index: 99999
+  background-color: rgba(0,0,0,0.4)
+._loading_box
+  position: relative
+  height: 60px
+  width: 60px
+
+._loading_item
+  display: inline-block
+  width: 8px
+  height: 8px
+  border-radius: 50%
+  background-color: white
+  opacity: 0.5
+  position: absolute
+  top: 50%
+  left: 50%
+  --angle: calc(360deg / 8 * var(--index))
+  --x: calc(cos(var(--angle)) * 30px)
+  --y: calc(sin(var(--angle)) * 30px)
+  translate: calc(var(--x) - 50%) calc(var(--y) - 50%)
+  --duration: 1.8s
+  --delay: calc(var(--duration) * var(--index) / 8)
+  animation: animate var(--duration) var(--delay) infinite ease-in-out
+
+@keyframes animate
+  50%
+    opacity: 1
+    transform: scale(1.6)
+</style>
--- packages/app/src/pages/RMComponent/RMComponent.vue
+++ packages/app/src/pages/RMComponent/RMComponent.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
 import { menu } from '../../layouts/index'
-import { ref } from 'vue'
+import { nextTick, ref } from 'vue'
 // import RMHamburger from 'src/components/RMHamburger/RMHamburger.vue'
 import RMHeader from 'src/components/RMHeader/RMHeader.vue'
 import { useRouter } from 'vue-router'
+import { useSpinner } from 'src/components/RMSpinner/RMSpinner'
 
 const router = useRouter()
 
@@ -16,6 +17,14 @@ const logout = () => {
 const menuClick = (menu: { name: string; url: string }) => {
   router.push({ path: menu.url })
 }
+
+const spinnerStart = () => {
+  useSpinner(async () => {
+    await nextTick(() => {
+      console.log('test')
+    })
+  })
+}
 </script>
 
 <template>
@@ -26,9 +35,15 @@ const menuClick = (menu: { name: string; url: string }) => {
       @logout="logout"
       @menuClick="menuClick"
     />
+    <div class="page_container">
+      <q-btn label="スピナーテスト" @click="spinnerStart" />
+    </div>
   </div>
 </template>
 
 <style lang="sass" scoped>
-// $
+.page_container
+  margin-top: 60px
+  height: 100svh
+  background: #707070
 </style>
